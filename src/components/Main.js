03d4c12cb6ee112518bcf21f95c2161b3dfed7e0
chipallino1 <skorupich00@mail.ru>
2019-12-05 15:58:33 +0300

    added project

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ControlledCarousel from "./ControlledCarousel";
+
+function Main() {
+    return (
+        <>
+            <ControlledCarousel/>
+        </>
+    );
+}
+
+export default Main;
