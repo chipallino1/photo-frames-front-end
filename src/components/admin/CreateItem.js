818d36820e5d882a4e363ab88220ad6749d4d67a
egsk <egsk@sam-solutions.com>
2019-12-11 18:06:51 +0300

    making two requests to create photo-frame

--- src/components/admin/CreateItem.js
+++ src/components/admin/CreateItem.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {SketchPicker} from 'react-color';
 import DateTimePicker from 'react-datetime-picker';
 import {API_BASE_URL} from "../../constants";
+import {createItem} from "../../util/APIUtils";
 
 class CreateItem extends Component {
     constructor(props) {
@@ -42,7 +43,6 @@ class CreateItem extends Component {
 
     handleSubmit = (event) => {
         let photoFramesDto = {};
-        photoFramesDto.file = this.state.selectedFile;
         photoFramesDto.name = this.state.name;
         photoFramesDto.vendorCode = this.state.vendorCode;
         photoFramesDto.borderMaterial = this.state.borderMaterial;
@@ -71,19 +71,20 @@ class CreateItem extends Component {
                 rgb: this.state.allColorsRgb[i]
             });
         }
-
-        const data = new FormData();
-        data.append('file', this.state.selectedFile);
         console.log(photoFramesDto);
-        fetch(API_BASE_URL + "/photo-frames/create", {
-            method: 'POST',
-            body: data
-        }).then(r => {
-            alert("Success");
-        })
-            .catch(r => {
-                console.log(r);
+        createItem(photoFramesDto).then(r => {
+            alert(r.id);
+            const data = new FormData();
+            data.append('file', this.state.selectedFile);
+            fetch(API_BASE_URL + "/photo-frames/addPhoto?id=" + r.id, {
+                method: 'POST',
+                body: data
+            }).then(r => {
+                alert("Success");
+            }).catch(r => {
+                alert(r);
             });
+        })
     }
 
     handleAddSize = () => {
@@ -120,97 +121,94 @@ class CreateItem extends Component {
             <div className="signup-container">
                 <div className="signup-content">
                     <h1 className="signup-title">Create new item here...</h1>
-                    <form action="http://localhost:8080/photo-frames/create" method="POST"
-                          encType="multipart/form-data">
-                        <div className="form-item">
-                            <input type="file" name="file"
-                                   className="form-control" placeholder="Photo" onChange={this.handleFileChange}
-                                   required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="text" name="name"
-                                   className="form-control" placeholder="Name"
-                                   value={this.state.name} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="text" name="vendorCode"
-                                   className="form-control" placeholder="Vendor code"
-                                   value={this.state.vendorCode} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="text" name="borderMaterial"
-                                   className="form-control" placeholder="Border material"
-                                   value={this.state.borderMaterial} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="number" name="insideMaterial"
-                                   className="form-control" placeholder="Inside material"
-                                   value={this.state.insideMaterial} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="number" name="thickness"
-                                   className="form-control" placeholder="Thickness"
-                                   value={this.state.thickness} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="number" name="cost"
-                                   className="form-control" placeholder="Cost"
-                                   value={this.state.cost} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="text" name="description"
-                                   className="form-control" placeholder="Description"
-                                   value={this.state.description} onChange={this.handleInputChange} required/>
-                        </div>
-                        <div className="form-item">
-                            <input type="text" name="currentSize"
-                                   className="form-control" placeholder="Size"
-                                   value={this.state.currentSize} onChange={this.handleInputChange}/>
-                            <div className="m-1">
-                                <button type="button" className="btn btn-outline-secondary"
-                                        onClick={this.handleAddSize}>Add size
-                                </button>
-                            </div>
-
-                            <input type="text" name="allSizes"
-                                   className="form-control" placeholder="Sizes"
-                                   value={this.state.allSizes} onChange={this.handleInputChange} disabled/>
-                        </div>
-                        <div className="form-item">
-                            <SketchPicker color={this.state.currentColorRgb} onChangeComplete={this.handleColorChange}/>
-                            <input type="text" name="currentColor"
-                                   className="form-control" placeholder="Color name"
-                                   value={this.state.currentColor} onChange={this.handleInputChange}/>
-                            <div className="m-1">
-                                <button type="button" className="btn btn-outline-secondary"
-                                        onClick={this.handleAddColor}>Add color
-                                </button>
-                            </div>
-
-                            <input type="text" name="allSizes"
-                                   className="form-control" placeholder="Colors"
-                                   value={this.state.allColors} onChange={this.handleInputChange} disabled/>
-                        </div>
-                        <div className="form-item">
-                            <input type="text" name="discount"
-                                   className="form-control" placeholder="Discount"
-                                   value={this.state.discount} onChange={this.handleInputChange}/>
-                        </div>
-                        <div className="form-group">
-                            <label>Start discount:</label>
-                            <DateTimePicker className="form-control" onChange={this.onChangeStartDate}
-                                            value={this.state.startDate} format="yyyy-MM-dd HH:mm"/>
-                        </div>
-                        <div className="form-group">
-                            <label>End discount:</label>
-                            <DateTimePicker className="form-control" onChange={this.onChangeEndDate}
-                                            value={this.state.endDate} format="yyyy-MM-dd HH:mm"/>
-                        </div>
-                        <div className="form-item">
-                            <button type="submit" className="btn btn-block btn-primary">Create
+                    <div className="form-item">
+                        <input type="file" name="file"
+                               className="form-control" placeholder="Photo" onChange={this.handleFileChange}
+                               required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="text" name="name"
+                               className="form-control" placeholder="Name"
+                               value={this.state.name} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="text" name="vendorCode"
+                               className="form-control" placeholder="Vendor code"
+                               value={this.state.vendorCode} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="text" name="borderMaterial"
+                               className="form-control" placeholder="Border material"
+                               value={this.state.borderMaterial} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="number" name="insideMaterial"
+                               className="form-control" placeholder="Inside material"
+                               value={this.state.insideMaterial} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="number" name="thickness"
+                               className="form-control" placeholder="Thickness"
+                               value={this.state.thickness} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="number" name="cost"
+                               className="form-control" placeholder="Cost"
+                               value={this.state.cost} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="text" name="description"
+                               className="form-control" placeholder="Description"
+                               value={this.state.description} onChange={this.handleInputChange} required/>
+                    </div>
+                    <div className="form-item">
+                        <input type="text" name="currentSize"
+                               className="form-control" placeholder="Size"
+                               value={this.state.currentSize} onChange={this.handleInputChange}/>
+                        <div className="m-1">
+                            <button type="button" className="btn btn-outline-secondary"
+                                    onClick={this.handleAddSize}>Add size
                             </button>
                         </div>
-                    </form>
+
+                        <input type="text" name="allSizes"
+                               className="form-control" placeholder="Sizes"
+                               value={this.state.allSizes} onChange={this.handleInputChange} disabled/>
+                    </div>
+                    <div className="form-item">
+                        <SketchPicker color={this.state.currentColorRgb} onChangeComplete={this.handleColorChange}/>
+                        <input type="text" name="currentColor"
+                               className="form-control" placeholder="Color name"
+                               value={this.state.currentColor} onChange={this.handleInputChange}/>
+                        <div className="m-1">
+                            <button type="button" className="btn btn-outline-secondary"
+                                    onClick={this.handleAddColor}>Add color
+                            </button>
+                        </div>
+
+                        <input type="text" name="allSizes"
+                               className="form-control" placeholder="Colors"
+                               value={this.state.allColors} onChange={this.handleInputChange} disabled/>
+                    </div>
+                    <div className="form-item">
+                        <input type="text" name="discount"
+                               className="form-control" placeholder="Discount"
+                               value={this.state.discount} onChange={this.handleInputChange}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Start discount:</label>
+                        <DateTimePicker className="form-control" onChange={this.onChangeStartDate}
+                                        value={this.state.startDate} format="yyyy-MM-dd HH:mm"/>
+                    </div>
+                    <div className="form-group">
+                        <label>End discount:</label>
+                        <DateTimePicker className="form-control" onChange={this.onChangeEndDate}
+                                        value={this.state.endDate} format="yyyy-MM-dd HH:mm"/>
+                    </div>
+                    <div className="form-item">
+                        <button type="button" className="btn btn-block btn-primary" onClick={this.handleSubmit}>Create
+                        </button>
+                    </div>
                 </div>
             </div>
 
