fd9973cea7b263d8d959c7a11c62ccc94854ae59
chipallino1 <skorupich00@mail.ru>
2020-03-02 17:51:59 +0300

    Item overview page updated. Added 'get info about frames'.

--- src/components/pages/ItemOverview.js
+++ src/components/pages/ItemOverview.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from 'react-image-gallery';
+import {Paper} from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 class ItemOverview extends React.Component {
 
@@ -20,16 +29,62 @@ class ItemOverview extends React.Component {
     ];
     showPlayButton = false;
 
-    render() {
 
+    render() {
         return (
             <div>
-                <div className="row" style={{margin:'0.5%'}}>
+                <div className="row" style={{margin: '0.5%'}}>
                     <div className="col-lg-8">
-                        <ImageGallery items={this.images} showPlayButton={this.showPlayButton}/>
+                        <Paper>
+                            <ImageGallery items={this.images} showPlayButton={this.showPlayButton}/>
+                        </Paper>
                     </div>
                     <div className="col-lg-4">
-                        Test
+                        <Paper style={{padding: '1%', marginBottom: '1%'}}>
+                            <h2>Photo frame name</h2>
+                            <h4>Стоимость: 300 BYN</h4>
+                            <h4>Материал: Дерево</h4>
+                            <FormControl style={{margin:'1%', minWidth:150}}>
+                                <InputLabel id="demo-simple-select-label">Цвет</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl style={{margin:'1%', minWidth:150}}>
+                                <InputLabel id="demo-simple-select-label">Размер</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <Typography variant="body1" gutterBottom>
+                                body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+                                unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+                                dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                            </Typography>
+                        </Paper>
+                        <Paper style={{padding: '1%', marginBottom: '1%'}}>
+                            <Typography variant="h6" gutterBottom>
+                                Хотите узнать есть в наличии эти рамки?
+                            </Typography>
+                            <Typography variant="subtitle1" gutterBottom>
+                                Просто укажите свой эмэйл и телефон нажмите кнопку узнать и мы сообщим вам о наличии!
+                            </Typography>
+                            <TextField id="standard-basic" label="Телефон" style={{margin:'1%'}}/>
+                            <TextField id="standard-basic" label="Email" style={{margin:'1%'}}/>
+                            <Button variant="contained" color="primary" style={{margin:'1%'}}>
+                                Узнать о наличии!
+                            </Button>
+                        </Paper>
                     </div>
                 </div>
             </div>
