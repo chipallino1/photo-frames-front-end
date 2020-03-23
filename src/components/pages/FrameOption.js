6ac50098580507463ce116d85f6a8d3d22512e3a
chipallino1 <skorupich00@mail.ru>
2020-03-23 16:28:31 +0300

    Button for adding photo updated.

--- src/components/pages/FrameOption.js
+++ src/components/pages/FrameOption.js
@@ -4,10 +4,44 @@ import Button from "@material-ui/core/Button";
 
 class FrameOption extends React.Component {
 
+    deleteStyleDisplayNone = {
+        marginTop: '5%',
+        marginLeft: '2%',
+        display: 'none'
+    };
+
+    deleteStyleDisplay = {
+        marginTop: '5%',
+        marginLeft: '2%',
+        display: ''
+    };
+
     constructor(props) {
         super(props);
+        this.state = {
+            photoButtonColor: "default",
+            photoButtonDeleteStyle: this.deleteStyleDisplayNone
+        };
+        this.handlePhotoChanged = this.handlePhotoChanged.bind(this);
+        this.handlePhotoDeleted = this.handlePhotoDeleted.bind(this);
     }
 
+
+    handlePhotoChanged() {
+        this.setState((state) => {
+            state.photoButtonColor = 'primary';
+            state.photoButtonDeleteStyle = this.deleteStyleDisplay;
+            return state;
+        });
+    }
+
+    handlePhotoDeleted() {
+        this.setState((state) => {
+            state.photoButtonDeleteStyle = this.deleteStyleDisplayNone;
+            state.photoButtonColor = 'default';
+            return state;
+        })
+    }
 
     render() {
         return <div className="w-50 mx-auto">
@@ -25,13 +59,26 @@ class FrameOption extends React.Component {
                     variant="contained"
                     component="label"
                     style={{marginTop: '5%'}}
+                    color={this.state.photoButtonColor}
                 >
                     <p>Фото</p>
                     <input
                         type="file"
                         style={{display: "none"}}
-                        onChange={this.props.handlePhotoChange}
+                        onChange={
+                            (event) => {
+                                this.handlePhotoChanged();
+                                this.props.handlePhotoChange(event);
+                            }
+                        }
                     />
+                </Button>
+                <Button
+                    variant="contained"
+                    component="label"
+                    style={this.state.photoButtonDeleteStyle}
+                    onClick={this.handlePhotoDeleted}>
+                    <p>Удалить</p>
                 </Button>
             </div>
 
